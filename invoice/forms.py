05285012cebbb2f30c8083bffaa53750506583b5
kamilw0909 <kwardach88@gmail.com>
2019-03-03 10:59:29 +0100

    Add new templates, views and urls for new invoice and edit invoice sites. Customize template new.html and edit.html for responsive web.

---
+++ invoice/forms.py
@@ -0,0 +1,24 @@
+from django.forms import ModelForm, inlineformset_factory, Textarea
+
+from .models import Invoice, Item
+
+
+class InvoiceForm(ModelForm):
+    class Meta:
+        model = Invoice
+        fields = ['invoice_number', 'invoice_date', 'invoice_sale_date',
+                  'invoice_payment_date', 'payment', 'invoice_s_fk',
+                  'invoice_b_fk', 'comments', 'sign']
+        widgets = {'comments': Textarea(attrs={'cols': 50, 'rows': 2})}
+
+
+class ItemForm(ModelForm):
+    class Meta:
+        model = Item
+        exclude = ()
+
+
+ItemFormSet = inlineformset_factory(Invoice, Item,
+                                    form=ItemForm,
+                                    extra=1,
+                                    can_delete=True)
--- invoice/views.py
+++ invoice/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
+from django.forms import inlineformset_factory, modelformset_factory
 
 from .to_word import main
-from .models import Invoice
+from .models import Invoice, Item
+from .forms import InvoiceForm, ItemFormSet
 
 
 class IndexView(generic.ListView):
@@ -27,12 +29,17 @@ def detail(request, invoice_id):
 
 
 def edit(request, invoice_id):
-    invoice = get_object_or_404(Invoice, pk=invoice_id)
+    invoice = Invoice.objects.get(pk=invoice_id)
+    form_invoice = InvoiceForm(instance=invoice)
+    formset_item = ItemFormSet(instance=invoice)
     return render(request,
                   'invoices/invoice/edit.html',
-                  {'invoice': invoice})
+                  {'form_invoice': form_invoice, 'formset_item': formset_item})
 
 
 def new_invoice(request):
+    form_invoice = InvoiceForm()
+    formset_item = ItemFormSet()
     return render(request,
-                  'invoices/invoice/new.html')
+                  'invoices/invoice/new.html',
+                  {'form_invoice': form_invoice, 'formset_item': formset_item})
